2fcbd9f28e6798d66a0c3e34215cfc022e215b26
bartek <bartekfr@wp.pl>
2013-09-10 17:27:18 +0200

    deleting key

--- scripts/mongolabKey.js
+++
@@ -1,8 +0,0 @@
-var app = app || {};
-(function() {
-	app.config = {
-		baseUrl: 'https://api.mongolab.com/api/1/databases/dict/collections/',
-		//use you MongoLab API key
-		params: '?apiKey=YourApiKey'
-	};
-})();
